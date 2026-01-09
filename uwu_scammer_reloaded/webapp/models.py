d2ee224f360287495912286270bc8f6f55fdfa86
JBDCE <61785226+JBDCE@users.noreply.github.com>
2026-01-09 18:23:40 +0100

    Remove django (#3)
    * Copy paste over contributions from issue
    
    Thank you gandie <3
    
    * Actually read the dockerfile and update package deployment to respect pipenv
    
    * get rid of old django project files
    
    * Move all fastapi files to the main folder
    
    * Update database.db file in gitignore
    
    * Update dockerfile to include the new paths
    
    Beep buup boop
    
    * Update readme removing the old django attempt tutorial
    
    * Improve readme formatting

--- uwu_scammer_reloaded/webapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class CreditCard(models.Model):
-    # TODO Figure out if this is large enough for this feature
-    cardnum = models.BigIntegerField(max_length=16)
-    expiry = models.DateField()
-    cvs = models.IntegerField(max_length=3)
